070473db32516f29301a942f0ac4920e8d3e87b0
youk720 <tjm.yk0259@gmail.com>
2018-11-16 11:20:46 +0900

    2018_11_16_1

--- bell_web.py
+++ bell_web.py
@@ -17,35 +17,39 @@ off_status = True
 while True:
     pin_status = GPIO.input(sw)
 
-    if pin_status == 1:
+    try:
+        if pin_status == 1:
         #ONの反応をsubprocessでcurlをmac側へ叩く
-        if on_status == True:
-            subprocess.Popen(['curl',
-                             '-X',
-                             'POST',
-                             'http://10.3.100.209:3000',
-                             '-H',
-                             'Accept: application/json',
-                             '-H',
-                             'Content-type: application/json',
-                             '-d',
-                             '{ "sw" : "True" }'])
-            on_status = False
-            off_status = True
-    if pin_status == 0:
-        #OFFの反応をsubprocessでcurlをmac側へ叩く
-        # subprocess.Popen('curl -X POST http://10.3.100.209:3000 -H "Accept: application/json" -H "Content-type: application/json" -d '{ "sw" : "False" }'', shell=True)
-        if off_status == True:
+            if on_status == True:
+                subprocess.Popen(['curl',
+                                 '-X',
+                                 'POST',
+                                 'IP',
+                                 '-H',
+                                 'Accept: application/json',
+                                 '-H',
+                                 'Content-type: application/json',
+                                 '-d',
+                                 '{ "sw" : "True" }'])
+                on_status = False
+                off_status = True
+        if pin_status == 0:
+            #OFFの反応をsubprocessでcurlをmac側へ叩く
+            # subprocess.Popen('curl -X POST http://10.3.100.209:3000 -H "Accept: application/json" -H "Content-type: application/json" -d '{ "sw" : "False" }'', shell=True)
+            if off_status == True:
 
-            subprocess.Popen(['curl',
-                             '-X',
-                             'POST',
-                             'http://10.3.100.209:3000',
-                             '-H',
-                             'Accept: application/json',
-                             '-H',
-                             'Content-type: application/json',
-                             '-d',
-                             '{ "sw" : "False" }'])
-            on_status = True
-            off_status = False
+                subprocess.Popen(['curl',
+                                 '-X',
+                                 'POST',
+                                 'IP',
+                                 '-H',
+                                 'Accept: application/json',
+                                 '-H',
+                                 'Content-type: application/json',
+                                 '-d',
+                                 '{ "sw" : "False" }'])
+                on_status = True
+                off_status = False
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+        print('\n' + "Over SW test")
